cc55805e08caf7fbad86de03e08afc993284b088
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2023-10-27 19:58:27 +0100

    filtered the home-patient

--- src/screens/Patients.jsx
+++ src/screens/Patients.jsx
@@ -155,47 +155,51 @@ const filteredPatients = patients ? patients.filter((patient) => {
       </div>
      </div>
         <div className="data-box grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-4 gap-y-8">
-          {filteredPatients.map((data) => (
-             <div
-             className="h-30 py-4 w-auto shadow-md rounded-lg flex flex-col items-start"
-             key={data?.id}
-           >
-             <div className="flex flex-row  justify-evenly px-4">
-             <div className={`text-center text-5xl h-20 w-20 px-2 py-1 items-center flex font-bold ${
-  data.selectedValue === "Home-Patient"
-    ? 'text-green-500'
-    : data.selectedValue === "Hospital-Calls"
-    ? 'text-purple-500'
-    : 'text-[#ff5162]'
-}`}>
+        {filteredPatients
+  .filter((data) => data.selectedValue !== "Home-Patient")
+  .map((data) => (
+    <div
+      className="h-30 py-4 w-auto shadow-md rounded-lg flex flex-col items-start"
+      key={data?.id}
+    >
+      <div className="flex flex-row  justify-evenly px-4">
+        <div
+          className={`text-center text-5xl h-20 w-20 px-2 py-1 items-center flex font-bold ${
+            data.selectedValue === "Hospital-Calls"
+              ? 'text-purple-500'
+              : 'text-[#ff5162]'
+          }`}
+        >
+          {data?.surname[0].toUpperCase()}
+        </div>
+        <div className="flex ml-4 space-y-2 flex-col">
+          <p className="text-sm">
+            Name:{' '}
+            <span className="text-gray-800">
+              {data?.surname} &nbsp; {data?.othername}
+            </span>
+          </p>
+          <p className="text-sm">
+            Clinicians:{' '}
+            <span className="text-gray-800">{data?.clinician}</span>
+          </p>
+          <p className="text-sm">
+            Date:{' '}
+            <span className="text-gray-800 text-xs gap-x-3 items-center">
+              {data?.dateRegistered}
+            </span>
+          </p>
+          <button
+            className="px-2 py-0.5 text-sm bg-[#FF5162] text-white"
+            onClick={() => onView(data?.id)}
+          >
+            View
+          </button>
+        </div>
+      </div>
+    </div>
+  ))}
 
-  {data?.surname[0].toUpperCase()}
-</div>
-               <div className="flex ml-4 space-y-2 flex-col">
-                 <p className="text-sm">
-                   Name:{' '}
-                   <span className="text-gray-800">{data?.surname} &nbsp; {data?.othername}</span>
-                 </p>
-                 <p className="text-sm">
-                   Clinicians:{' '}
-                   <span className="text-gray-800">{data?.clinician}</span>
-                 </p>
-                 <p className="text-sm">
-                   Date:{' '}
-                   <span className="text-gray-800 text-xs gap-x-3 items-center">
-                     {data?.dateRegistered}
-                   </span>
-                 </p>
-                 <button className="px-2 py-0.5 text-sm bg-[#FF5162] text-white" onClick={()=>onView(data?.id)}>
-                   View
-                 </button>
-               </div>
-               {/* <div className="deletebutton ml-5" onClick={onDelete}>
-                 <ImBin size={20} className="text-[#ff5162] cursor-pointer" />
-               </div> */}
-             </div>
-           </div>
-          ))}
         </div>
         <div className="fixed bottom-4 right-4 h-40 w-40 cursor-pointer bg-white flex justify-center items-center rounded-full shadow-lg">
           <Link to={'/add-new'}>
