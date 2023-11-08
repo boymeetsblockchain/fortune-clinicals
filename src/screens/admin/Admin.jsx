0c06ae03c3ce639a0dc164f60ee0fe23497080b2
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2023-11-08 14:45:42 +0100

    fixed the admin dashboard responsiveness

--- src/screens/admin/Admin.jsx
+++ src/screens/admin/Admin.jsx
@@ -16,7 +16,7 @@ const menuItems = [
 ];
 function Admin() {
   return (
-  <div className='flex items-center flex-col  justify-center h-screen px-4 md:px-8 lg:px-12'>
+  <div className='flex items-center flex-col  justify-center h-full py-4 px-4 md:px-8 lg:px-12'>
   <h1 className="text-center md:text-4xl text-2xl  mb-4 font-bold">Welcome, Admin</h1>
   <div className="grid  grid-cols-1 md:grid-cols-2  md:gap-6 gap-2">
   {
