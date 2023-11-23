73c11e0cd7efbb6a917a2daa3a1c29db58311c87
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2023-11-23 03:32:07 +0100

    added a new feature

--- src/components/BreakDown.jsx
+++ src/components/BreakDown.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import useMonthsData from '../hooks/useMonthData';
 import { useParams } from 'react-router-dom';
-import AdminNav from './AdminNav'
+import AdminNav from './AdminNav';
 import Loader from './Loader';
 import { useNavigate } from 'react-router-dom';
 
@@ -27,20 +27,18 @@ function BreakDown({ match }) {
     <>
       <AdminNav />
       <div className="px-4 md:px-8 lg:px-8 h-full mx-auto my-5">
-        <h1 className="text-2xl font-semibold mb-4 text-center ">{selectedMonth?.name} Daily Transactions</h1>
+        <h1 className="text-2xl font-semibold mb-4 text-center">{selectedMonth?.name} Daily Transactions</h1>
         <h1 className='text-2xl font-bold mb-4 text-center'> Total Payment for {selectedMonth?.name}: {""} &#8358;{selectedMonth.totalPaymentsForMonth}</h1>
         {selectedMonth.dailyPayments.map((dayData) => (
           <div key={dayData.day} className="mb-4 p-4 rounded-lg">
             <h2 className="text-xl font-semibold mb-2">{`Day ${dayData.day}`}</h2>
+            <p className="text-lg mb-2 font-bold ">Total Payment: &#8358;{dayData.payments.reduce((total, payment) => total + parseFloat(payment.amount), 0)}</p>
             <ul className='flex flex-col space-y-4 '>
               {dayData.payments.map((payment, index) => (
-                <li key={index} className="flex items-center   bg-[#FF5162] text-white  py-3 justify-between mb-2 cursor-pointer" onClick={() => handlePatientClick(payment.patientId)}>
-                  <div className='flex justify-between  '>
-            
+                <li key={index} className="flex items-center bg-[#FF5162] text-white py-3 justify-between mb-2 cursor-pointer" onClick={() => handlePatientClick(payment.patientId)}>
+                  <div className='flex justify-between'>
                     <h1 className="ml-4 text-2xl ">&#8358; {payment.amount}</h1>
-                
                   </div>
-               
                 </li>
               ))}
             </ul>
