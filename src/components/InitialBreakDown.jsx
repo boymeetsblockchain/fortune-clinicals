526012ce7a83990114bdc43f55c935c0bbd788f2
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2025-01-15 12:42:33 +0100

    added new days

--- src/components/InitialBreakDown.jsx
+++ src/components/InitialBreakDown.jsx
@@ -50,7 +50,10 @@ function InitialBreakDown() {
             key={dayData.day}
             className="mb-4 p-4 rounded-lg bg-gray-100 shadow"
           >
-            <h2 className="text-xl font-semibold mb-2">{`Day ${dayData.day}`}</h2>
+            <div className="flex items-center justify-between">
+              <h2 className="text-xl font-semibold mb-2">{`Day ${dayData.day}`}</h2>
+              <h2 className="text-xl font-semibold mb-2">{`Total Initial Review for the day: ${dayData.reviews.length} `}</h2>
+            </div>
             {dayData.reviews.length > 0 ? (
               <ul className="flex flex-col space-y-4">
                 {dayData.reviews.map((patient, index) => (
