f19ad3f67ec3a7ae50f22c8edd37306f3846ab7d
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2025-02-15 19:13:34 +0100

    added new functions

--- src/components/InitialReview.jsx
+++ src/components/InitialReview.jsx
@@ -1,24 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import Input from './Input';
-import { collection, where, query, getDocs, addDoc, deleteDoc, doc } from 'firebase/firestore';
-import { db } from '../firebase.config';
-import ComponentLoader from './ComponentLoader';
-import toast from 'react-hot-toast';
+import React, { useState, useEffect } from "react";
+import Input from "./Input";
+import {
+  collection,
+  where,
+  query,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
+import { db } from "../firebase.config";
+import ComponentLoader from "./ComponentLoader";
+import toast from "react-hot-toast";
 
 function InitialReview({ patientId }) {
-  const [comment, setComment] = useState('');
+  const [comment, setComment] = useState("");
   const [reviews, setReview] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [date, setDate] = useState('');
+  const [date, setDate] = useState("");
 
   // Function to fetch session details based on patientId
   const reviewDetails = async () => {
     try {
-      const reviewsQuery = query(collection(db, 'reviews'), where('patientId', '==', patientId));
+      const reviewsQuery = query(
+        collection(db, "reviews"),
+        where("patientId", "==", patientId)
+      );
       const querySnapshot = await getDocs(reviewsQuery);
 
       // Use map to directly transform querySnapshot to an array of reviewDetails
-      const reviewDetails = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      const reviewDetails = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
 
       reviewDetails.sort((a, b) => {
         const dateA = new Date(a.date);
@@ -26,12 +40,11 @@ function InitialReview({ patientId }) {
         return dateB - dateA;
       });
 
-
       // Update the payments state with fetched session details
       setReview(reviewDetails);
       setLoading(false);
     } catch (error) {
-      console.error('Error fetching session details:', error);
+      console.error("Error fetching session details:", error);
     }
   };
 
@@ -46,74 +59,90 @@ function InitialReview({ patientId }) {
     const paymentData = {
       comment,
       patientId,
-      date
+      date,
     };
 
     try {
-      const data = await addDoc(collection(db, 'reviews'), paymentData);
-      console.log('Session added with ID:', data.id);
+      const data = await addDoc(collection(db, "reviews"), paymentData);
+      console.log("Session added with ID:", data.id);
 
       // After adding a new session, refetch the session details to include the new one
       reviewDetails();
 
       // Clear the input fields
-      setComment('');
+      setComment("");
     } catch (error) {
-      console.error('Error adding session:', error);
+      console.error("Error adding session:", error);
     }
   };
 
   const deleteReview = async (reviewId) => {
     // Display a confirmation dialog
-    const isConfirmed = window.confirm('Are you sure you want to delete this review?');
-  
+    const isConfirmed = window.confirm(
+      "Are you sure you want to delete this review?"
+    );
+
     if (!isConfirmed) {
       return; // If the user clicks "Cancel" in the confirmation dialog, do nothing
     }
-  
+
     try {
-      await deleteDoc(doc(db, 'reviews', reviewId));
-      toast.success('Review deleted successfully');
-  
-      
+      await deleteDoc(doc(db, "reviews", reviewId));
+      toast.success("Review deleted successfully");
+
       reviewDetails();
     } catch (error) {
-      console.error('Error deleting review:', error);
+      console.error("Error deleting review:", error);
     }
   };
-  
 
   if (loading) {
     return (
-      <div className='flex item-center justify-center'>
+      <div className="flex item-center justify-center">
         <ComponentLoader />
       </div>
     );
   }
 
   return (
-    <div className='flex justify-center my-2 gap-y-3 flex-col px-3'>
-      <h1 className='text-center font-bold'>Review Info</h1>
-      {
-  reviews.length === 0 && (
-    <form className='space-y-2 flex flex-col' onSubmit={addNewPayment}>
-      <Input type={"text"} label={"Comment"} value={comment} onChange={(e) => setComment(e.target.value)} />
-      <Input label={"Date Registered"} type={"date"} value={date} onChange={(e) => setDate(e.target.value)} />
-      <button type="submit" className='bg-[#FF5162] text-white py-1.5'>Submit</button>
-    </form>
-  )
-}
-<div className="session-details-container">
+    <div className="flex justify-center my-2 gap-y-3 flex-col px-3">
+      <h1 className="text-center font-bold">Review Info</h1>
+
+      <form className="space-y-2 flex flex-col" onSubmit={addNewPayment}>
+        <Input
+          type={"text"}
+          label={"Comment"}
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+        />
+        <Input
+          label={"Date Registered"}
+          type={"date"}
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        />
+        <button type="submit" className="bg-[#FF5162] text-white py-1.5">
+          Submit
+        </button>
+      </form>
+
+      <div className="session-details-container">
         <div className="session-details">
           {reviews.map((session) => (
-            <div key={session.id} className="flex space-y-2 justify-between bg-white rounded-lg p-3 shadow-md mb-3">
+            <div
+              key={session.id}
+              className="flex space-y-2 justify-between bg-white rounded-lg p-3 shadow-md mb-3"
+            >
               <div className="flex flex-col">
                 <p className="text-gray-700 text-lg">{session?.comment}</p>
-                <p className="text-green-600 text-sm font-semibold"> Date: {session?.date}</p>
+                <p className="text-green-600 text-sm font-semibold">
+                  {" "}
+                  Date: {session?.date}
+                </p>
               </div>
               <button
                 onClick={() => deleteReview(session.id)}
-                className='bg-red-500 text-white py-1 px-2 ml-3 rounded-md'
+                className="bg-red-500 text-white py-1 px-2 ml-3 rounded-md"
               >
                 Delete
               </button>
@@ -125,4 +154,4 @@ function InitialReview({ patientId }) {
   );
 }
 
-export default InitialReview
+export default InitialReview;
